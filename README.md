7756d48e925b9ed350632d557723fe6dc8a2b535
SnoopyCodeX <extremeclasherph@gmail.com>
2024-09-11 13:24:38 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,50 @@
+![banner](./pictures/banner.png)
+
+## Auto Poke Back (Facebook web crawler)
+
+A simple web crawler made using [PuppeteerJS](https://pptr.dev) and NodeJS that automates pressing the "Poke Back" button whenever a facebook friend/user pokes you.
+
+### Setup
+
+First, run:
+
+```bash
+npm install
+```
+
+Then, run:
+
+```bash
+
+npx @puppeteer/browsers install chrome-headless-shell@latest
+```
+
+After that, open and edit the <kbd>.env</kbd> file and replace the keys with their respective values, however, the `USER_AGENT` key's value is optional and may be left as-is.
+
+```bash
+# Your facebook email address
+FB_EMAIL=youremail@gmail.com
+
+# Your facebook password
+FB_PASS=yourpassword123
+
+# Browser user agent (optional)
+USER_AGENT='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 Edg/123.0.2420.81'
+```
+
+Once all are done, you may then run:
+
+```bash
+node index.js
+```
+
+**NOTE:** If you encounter a checkpoint, just open your facebook account in your another device and check your notification for a login approval request. Click it and just approve your login then wait for a few seconds and this tool will detect the approval. However, facebook's login checkpoint may also mean that your account has been marked as spam or temporarily disabled by facebook.
+
+### Preview
+![preview](./pictures/preview.png)
+
+### Possible Improvements
+- [ ] Login cookie (to avoid relogging in and going through the login checkpoint everytime)
+
+### Disclaimer
+I will not be responsible for any damages/consequences that this may cause to your and/or other's account(s). Please use at your own risk.
---
+++ index.js
@@ -0,0 +1,103 @@
+const puppeteer = require('puppeteer');
+const dotenv = require('dotenv');
+const ansis = require('ansis');
+const figlet = require('figlet');
+dotenv.config();
+
+(async () => {
+    console.log(ansis.cyanBright(figlet.textSync('AutoPokeBack', 'Chunky')));
+    console.log(ansis.cyan.bold(`Made by: SnoopyCodeX @https://github.com/SnoopyCodeX/autopokeback\n`));
+
+    const delay = (time) => new Promise(resolve => setTimeout(resolve, time));
+    const browser = await puppeteer.launch({ headless: 'shell', protocolTimeout: 1000 * 60 * 60 * 24 });
+    const page = await browser.newPage();
+
+    // Custom SIGINT event for windows
+    if (process.platform == 'win32') {
+        const rl = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+
+        rl.on('SIGINT', () => process.emit('SIGINT'));
+    }
+
+    // Gracefully shutdown headless browser when 
+    // user exits using CTRL + C
+    process.on('SIGINT', async () => {
+        await browser.close();
+        console.log(ansis.red.italic('Process terminated, exiting...'));
+        process.exit(1);
+    });
+
+    // ===========================[ START ]===========================
+    console.log(ansis.italic.gray('Trying to log in...'));
+    await page.goto('https://www.facebook.com/', { waitUntil: 'networkidle2' });
+    console.log(ansis.italic.gray('Facebook loaded, logging in...'));
+
+    await page.type('#email', process.env.FB_EMAIL);
+    await page.type('#pass', process.env.FB_PASS);
+    await page.click('button[name="login"]');
+    await page.waitForNavigation({ waitUntil: 'networkidle2' });
+    let url = page.url();
+
+    // Checkpoint detection (Login approval)
+    if (url.includes('checkpoint')) {
+        console.log(ansis.red.italic(`\nCheckpoint detected, open your facebook notification to approve this login...`));
+        console.log(ansis.grey.italic('Waiting for login approval...\n'));
+        await page.screenshot({ path: './screenshots/checkpoint.png' });
+
+        // Wait for approval
+        while (url.includes('checkpoint')) {
+            await delay(3000);
+            url = page.url();
+        }
+
+        // Wait for main element to display (If I remember correctly, this element is only present on the home page after logging in)
+        const mainElement = await page.$('div[role=main]');
+
+        // If main element is null/undefined, exit the process because this might mean that the user failed to
+        // approve this login
+        if (!mainElement) {
+            console.log(ansis.red.italic(`Failed to approve this login, exiting...`));
+            await browser.close();
+            process.exit(1);
+        }
+
+        // Login has been approved successfully
+        console.log(ansis.grey.italic('Login has been approved...'));
+    }
+
+    console.log(ansis.italic.gray('Logged in, redirecting to pokes page...'));
+    await page.setUserAgent(process.env.USER_AGENT);
+    await page.goto('https://www.facebook.com/pokes', { waitUntil: 'networkidle2' });
+    console.log(ansis.italic.gray('Opened pokes page...\n'));
+
+    while(true) {
+        const pokeBackButtons = await page.$$(`div[aria-label="Poke Back"]`);
+        const pokeBackAnchors = await page.$$(`span > a[role='link'][target='_blank']`);
+        let usersToPokeBack = pokeBackButtons.length;
+
+        const message = `Found ${usersToPokeBack} user${usersToPokeBack > 1 || usersToPokeBack == 0 ? 's' : ''} to poke back`;
+        console.log(usersToPokeBack > 0 ? ansis.green(message) : ansis.italic.dim.red(message));
+
+        for (let i = 0; i < usersToPokeBack; i++) {
+            // Get name of the user
+            const userName = await page.evaluate((el) => el.textContent, pokeBackAnchors[i]);
+
+            // Click "Poke Back"
+            await pokeBackButtons[i].click();
+            console.log(`[${i + 1}]: ${ansis.green.bold(userName)} ${ansis.reset.green('has been poked back!')}`);
+
+            // Delay for 1s
+            await delay(1000);
+        }
+
+        if (usersToPokeBack > 0)
+            console.log(ansis.green('\nAll users has been poked back!'));
+
+        console.log(ansis.italic.gray('\nRechecking for users to poke back...'));
+        usersToPokeBack = 0;
+        await delay(2000);
+    }
+})();
